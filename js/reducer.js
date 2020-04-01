856bd4cd08454ee451bd8916c692f95f023814d4
LucyWeltner <lucyiweltner@gmail.com>
2020-04-01 16:23:45 -0400

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,3 +1,6 @@
+let state = {count: 0}
+let action = {type: 'INCREASE_COUNT'}
+
 function changeState(state, action){
   switch (action.type) {
     case 'INCREASE_COUNT':
@@ -7,7 +10,14 @@ function changeState(state, action){
   }
 }
 
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
+function render() {
+	document.body.innerHTML = `<p>${state.count}</p>`
+}
 
-changeState(state, action)
+function dispatch(action){
+	state = changeState(state, action)
+	render()
+}
+
+dispatch(action)
+dispatch(action)
